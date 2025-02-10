3740d0674e34615979a48c0c3c947f1c916e913c
caothien2001 <thien.cao@apps-cyclone.com>
2025-02-10 15:20:28 +0700

    Improve order

--- src/modules/order/dtos/order.dto.ts
+++ src/modules/order/dtos/order.dto.ts
@@ -1,10 +1,14 @@
 import { BaseReqDTO } from '../../../base/base.dto'
 import { Expose } from 'class-transformer'
 import { OrderStatusTypes } from '../types/order-status.type'
+import { generateRandomString } from '../../../utils'
 
 export class OrderDTO extends BaseReqDTO {
     @Expose()
     userId: string
+
+    @Expose()
+    orderId: string
 
     @Expose()
     packageId: string
@@ -17,6 +21,7 @@ export class OrderDTO extends BaseReqDTO {
 
     init(data: OrderDTO) {
         this.userId = data.userId
+        this.orderId = generateRandomString(12)
         this.packageId = data.packageId
         this.price = data.price
         this.status = OrderStatusTypes.Pending
